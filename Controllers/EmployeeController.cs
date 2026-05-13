d117c11bee6a71ad37b561c497683235f596f0f7
Radhi2422 <106824237+Radhi2422@users.noreply.github.com>
2026-05-13 08:50:03 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]   
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null) { 
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id,Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(employee).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null) { return NotFound(); }
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Department { get; set; }
+        public int Salary { get; set; }
+    }
+}
+
+
+/*
+ 
+ */
